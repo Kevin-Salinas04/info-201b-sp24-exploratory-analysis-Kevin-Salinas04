5e6066ab10b92a6e8b781114f2236331adc9a3ea
Kevin-Salinas04 <165828996+Kevin-Salinas04@users.noreply.github.com>
2024-05-22 21:11:05 -0700

    Add files via upload

---
+++ data3.R
@@ -0,0 +1,40 @@
+Group	Sleep 6 hours or less	Sleep 7 hours or more
+18-29 year olds	
+46%
+54%
+30-49 year olds	
+44%
+56%
+50-64 year olds	
+38%
+62%
+65+ year olds	
+33%
+67%
+Men	
+39%
+61%
+Women	
+41%
+59%
+Employed	
+43%
+57%
+Not employed	
+38%
+62%
+Less than $30,000 annual household income	
+48%
+52%
+Between $30,000 to $75,000 annual household income	
+39%
+61%
+$75,000 or more annual household income	
+34%
+66%
+Have children under 18	
+46%
+54%
+Don't have children under 18	
+38%
+62%
